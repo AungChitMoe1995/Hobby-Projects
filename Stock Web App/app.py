7446f5c5369312a44f2be8bb8ca63638f404faaf
AungChitMoe1995 <aungchitmoe.um2@gmail.com>
2025-10-03 16:28:35 +0630

    hello

--- Stock Web App/app.py
+++ Stock Web App/app.py
@@ -1,16 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # create the app
 app = Flask(__name__)
 
 # define a route
+
+
 @app.route("/")
 def home():
-    return "Hello I am ACM🚀"
+    return render_template("home.html", title= "Home Page")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title = "About")
+
 
 @app.route("/<username>")
 def greetuser(username):
-    return f"Hello {username}"
+    capatilized_username = username.upper()
+    greetwithusername = f"Hello {capatilized_username}"
+    return """
+    <h1>greetwithusername</h1>
+    """
+
+@app.route("/multiply/<int:a>/<int:b>")
+def multiply(a, b):
+    result = a*b
+    return f"The result is {result}."
+
+
 # run the server
 if __name__ == "__main__":
     app.run(debug=True)
