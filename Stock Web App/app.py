6bd5abf3cd4231428cb60c4d59a7bc2005e42893
AungChitMoe1995 <aungchitmoe.um2@gmail.com>
2025-10-03 15:12:02 +0630

    J

--- Stock Web App/app.py
+++ Stock Web App/app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+
+# create the app
+app = Flask(__name__)
+
+# define a route
+@app.route("/")
+def home():
+    return "Hello I am ACM🚀"
+
+@app.route("/<username>")
+def greetuser(username):
+    return f"Hello {username}"
+# run the server
+if __name__ == "__main__":
+    app.run(debug=True)
 
